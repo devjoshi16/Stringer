7f6e1f6eb455419a773ec6f6724751c947e8e3b4
devjoshi16 <200303105078@paruluniversity.ac.in>
2023-05-31 20:29:25 +0530

    Added icon and seo

---
+++ src/components/Alert.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Alert(props) {
+    const capetalize = (word)=>{
+        const lower=word.toLowerCase();
+        return lower.charAt(0).toUpperCase()+lower.slice(1);
+    }
+  return (
+    <div style={{height:'50px'}}>
+    {props.alert&&<div
+      className={`alert alert-${props.alert.type} alert-dismissible fade show`}
+      role="alert"
+    >
+      <strong>{capetalize( props.alert.type)}</strong>:{props.alert.msg}
+    </div>}
+    </div>
+  );
+}
